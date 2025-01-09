cf17ff79dbea31a6323ba162415b8b2c50956948
DUMBUDUMBU <diana.duumbu@gmail.com>
2025-01-09 05:29:27 -0800

    changes

--- server.js
+++ server.js
@@ -10,8 +10,8 @@ const PORT = process.env.PORT || 5000;
 
 // Middleware
 app.use(cors());
-app.use(express.json()); // Parse JSON bodies
-app.use(bodyParser.json()); // Parse JSON bodies
+app.use(express.json()); 
+app.use(bodyParser.json()); 
 
 // Define routes
 app.get('/', (req, res) => {
@@ -19,7 +19,7 @@ app.get('/', (req, res) => {
 });
 
 // Use the messaging API
-app.use('/api/messages', messageHandler); // Send email route
+app.use('/api/messages', messageHandler); 
 
 // Start the server
 app.listen(PORT, () => {
